2c005035455a54cd5b9ab09306a35a49a9be7424
maheenakbar8 <maheenakbar000@gmail.com>
2026-08-12 01:59:24 +0500

    add Pydantic alert model

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Alert(BaseModel):
+    id: int
+    title: str
+    severity: str
+
 
 alerts = [
-    {
-        "id": 1,
-        "title": "Heavy Rain Warning",
-        "severity": "high"
-    },
-    {
-        "id": 2,
-        "title": "Road Closure",
-        "severity": "medium"
-    },
-    {
-        "id": 3,
-        "title": "Power Outage",
-        "severity": "low"
-    }
+    Alert(
+        id=1,
+        title="Heavy Rain Warning",
+        severity="high"
+    ),
+    Alert(
+        id=2,
+        title="Road Closure",
+        severity="medium"
+    ),
+    Alert(
+        id=3,
+        title="Power Outage",
+        severity="low"
+    )
 ]
 
 
@@ -27,6 +33,6 @@ def home():
     return {"message": "AlertHub is running!"}
 
 
-@app.get("/alerts")
+@app.get("/alerts", response_model=list[Alert])
 def get_alerts():
     return alerts
