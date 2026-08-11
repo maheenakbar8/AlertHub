9b9aa5aec1ff5c7d75ca5f152a2d44d897a9ec86
maheenakbar8 <maheenakbar000@gmail.com>
2026-08-12 02:06:58 +0500

    complete alert CRUD operations

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -54,3 +54,32 @@ def create_alert(alert: AlertCreate):
     alerts.append(new_alert)
 
     return new_alert
+
+
+@app.patch("/alerts/{alert_id}", response_model=Alert)
+def update_alert(alert_id: int, updated_alert: AlertCreate):
+    for index, alert in enumerate(alerts):
+        if alert.id == alert_id:
+            updated = Alert(
+                id=alert_id,
+                title=updated_alert.title,
+                severity=updated_alert.severity
+            )
+
+            alerts[index] = updated
+            return updated
+
+    raise HTTPException(status_code=404, detail="Alert not found")
+
+
+@app.delete("/alerts/{alert_id}")
+def delete_alert(alert_id: int):
+    for index, alert in enumerate(alerts):
+        if alert.id == alert_id:
+            deleted_alert = alerts.pop(index)
+            return {
+                "message": "Alert deleted",
+                "alert": deleted_alert
+            }
+
+    raise HTTPException(status_code=404, detail="Alert not found")
